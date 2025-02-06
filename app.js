573acf213c8a7d29db2ea511d18faf0e7cc84ec9
esimo13 <obak139@gmail.com>
2025-02-06 15:42:06 +0600

    tls

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const auctionRoutes = require("./routes/auctionRoutes");
+const { log } = require("console");
 
 const app = express();
 
 // Connect to MongoDB
 // mongoose.connect("mongodb://localhost:27017/auctionApp");
-mongoose.connect(
-  "mongodb+srv://auctionapp:auctionapp@cluster0.4sidi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  {
-    ssl: true,
-    tlsAllowInvalidCertificates: true,
-  }
-);
+mongoose
+  .connect(
+    "mongodb+srv://auctionapp:auctionapp@cluster0.4sidi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      tls: true,
+      tlsInsecure: false,
+    }
+  )
+  .then(() => console.log("connected tls"))
+  .catch((err) => console.error(err));
 
 app.use(cors());
 app.use(bodyParser.json());
